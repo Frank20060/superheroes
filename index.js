e05d62d8e8fddd58d538ddd5d2ba2dd97ebb0376
Frank20060 <159519815+Frank20060@users.noreply.github.com>
2025-02-17 16:24:52 +0000

    finish

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+// Imprime un mensaje en la consola
 console.log("Hola Superheroe")
 
+// Define un array de objetos que contiene información sobre varios superhéroes
 const bd=[
     {
         nombre: "MAGNETO",
@@ -23,13 +25,15 @@ const bd=[
     },
 ];
 
-let targetas = "";
-
-
+// Define una función para mostrar los superhéroes en la página
 function mostrarSupereroes(){
-
+    // Inicializa una cadena vacía para almacenar las tarjetas HTML
+    let targetas = "";
+    // Recorre el array de superhéroes
     for(let i = 0; i< bd.length; i++){
+        // Imprime en la consola el nombre y la URL de la imagen del superhéroe actual
         console.log("i: ", bd[i].nombre, bd[i].url_imagen);
+        // Añade una tarjeta HTML para el superhéroe actual a la cadena
         targetas= targetas + `
         <div class="tarjeta">
         <div class="imagen">
@@ -39,9 +43,46 @@ function mostrarSupereroes(){
         </div>
         `
     }
-
+    // Inserta las tarjetas HTML en el contenedor de la página
     document.querySelector(".contenedor").innerHTML = targetas;
-
 }
+
+// Llama a la función para mostrar los superhéroes cuando se carga la página
 mostrarSupereroes()
+
+// Selecciona el botón de la página
+const botton = document.querySelector("button");
+
+// Añade un evento de clic al botón
+botton.addEventListener("click", function(e){
+    // Previene el comportamiento por defecto del botón (submit)
+    e.preventDefault();
+    // Imprime un mensaje en la consola cuando se hace clic en el botón
+    console.log("Se ha hecho click en el boton");
+
+    // Captura el valor del campo de entrada del nombre del superhéroe
+    let nombre = document.querySelector("#nombreSuper").value;
+    // Captura el valor del campo de entrada de la URL de la imagen del superhéroe
+    let url = document.querySelector("#url_super").value;
+    // Imprime en la consola el nombre y la URL capturados
+    console.log("Nombre y url", nombre, url)
+    
+    // Crea un nuevo objeto con los valores capturados
+    const nuevoSuper =
+    {
+        nombre:nombre,
+        url_imagen:url,
+    }
+    
+    // Imprime en la consola el nuevo superhéroe
+    console.log("Nuevo superheroe", nuevoSuper);
+
+    // Añade el nuevo superhéroe al array de superhéroes
+    bd.push(nuevoSuper);
+    // Imprime en la consola la base de datos actualizada
+    console.log("Base de datos actualizada", bd);
+    // Llama a la función para mostrar los superhéroes actualizados en la página
+    mostrarSupereroes()
+});
+
 
